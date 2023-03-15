c55c3753f5de859bba4715f32bc11a7abafcf143
BalazsM <cubie.melchior@gmail.com>
2023-03-15 11:57:44 +0100

    Grid size, optimization

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// TODO: object seed
+// TODO: move objects (bounce)
+// TODO: objects count
 
 // --  globals  --------------------------------------------------------------
 
@@ -7,8 +10,94 @@ let leftViewpoint;
 let rightViewpoint;
 let topViewpoint;
 let bottomViewpoint;
+let gridSize;
 let grid;
 let objects;
+
+let pX;
+let pY;
+let pZ;
+
+let horizontalY;
+let verticalX;
+let leftX;
+let rightX;
+let topY;
+let bottomY;
+
+// ---------------------------------------------------------------------------
+
+function updateGridSize() {
+	grid = new Array(gridSize);
+	for (let x = 0; x < gridSize; x++) {
+		grid[x] = new Array(gridSize);
+		for (let y = 0; y < gridSize; y++) {
+			grid[x][y] = new Array(gridSize);
+			for (let z = 0; z < gridSize; z++) {
+				grid[x][y][z] = {
+					x: 0,
+					y: 0
+				};
+			}
+		}
+	}
+
+	pX = new Array(gridSize);
+	pY = new Array(gridSize);
+	pZ = new Array(gridSize);
+}
+
+function updateGrid() {
+	horizontalY = height * horizontalLine / 100;
+	verticalX = width * verticalLine / 100;
+	leftX = width * leftViewpoint / 100;
+	rightX = width * (100 + rightViewpoint) / 100;
+	topY = height * topViewpoint / 100;
+	bottomY = height * (100 + bottomViewpoint) / 100;
+	
+	for (let i = 0; i < gridSize; i++) {
+		pZ[i] = {
+			x : verticalX,
+			y : topY + (bottomY - topY) / (gridSize - 1) * i
+		};
+	}
+
+	for (let z = 0; z < gridSize; z++) {
+		for (let i = 0; i < gridSize; i++) {
+			pX[i] = {
+				x : pZ[z].x + (leftX - pZ[z].x) / (gridSize - 1) * i,
+				y : pZ[z].y + (horizontalY - pZ[z].y) / (gridSize - 1) * i
+			};
+			pY[i] = {
+				x : pZ[z].x + (rightX - pZ[z].x) / (gridSize - 1) * i,
+				y : pZ[z].y + (horizontalY - pZ[z].y) / (gridSize - 1) * i
+			};
+		}
+
+		let g = grid[0][0][z];
+		g.x = pZ[z].x;
+		g.y = pZ[z].y;
+		for (let x = 1; x < gridSize; x++) {
+			g = grid[x][0][z];
+			g.x = pX[x].x;
+			g.y = pX[x].y;
+			for (let y = 1; y < gridSize; y++) {
+				g = grid[0][y][z];
+				g.x = pY[y].x;
+				g.y = pY[y].y;
+
+				let p = intersectLines(pX[x].x, pX[x].y, 
+					rightX, horizontalY, 
+					pY[y].x, pY[y].y,
+					leftX, horizontalY);
+
+				g = grid[x][y][z];
+				g.x = p.x;
+				g.y = p.y;
+			}
+		}
+	}
+}
 
 // -- event handlers  --------------------------------------------------------
 
@@ -16,34 +105,37 @@ function setup() {
 	let canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent('workspace');
 
-	grid = new Array(10);
-	for (let x = 0; x < 10; x++) {
-		grid[x] = new Array(10);
-		for (let y = 0; y < 10; y++) {
-			grid[x][y] = new Array(10);
-			for (let z = 0; z < 10; z++) {
-//				grid[x][y][z] = random(10) > 5; // TODO: parameters
-				grid[x][y][z] = {
-					x: 0,
-					y: 0,
-					b: 0
-				};
-			}
-		}
-	}
-	grid[0][0][0].b = 1;
-//	console.log(grid);
-
-	objects = [];
-	for (let i = 0; i < 3; i++) {
-		objects[i] = {
+	objects = new Array();
+	for (let i = 0; i < 3; i++) { // TODO: 3-> number of objects
+		objects.push({
 			vertices: [
-				{x: 0, y: 0, z: 0},
-				
+				{ x: 0, y: 0, z: 0 },
+				{ x: 1, y: 0, z: 0 },
+				{ x: 1, y: 0, z: 1 },
+				{ x: 0, y: 0, z: 1 },
+				{ x: 0, y: 1, z: 0 },
+				{ x: 1, y: 1, z: 0 },
+				{ x: 1, y: 1, z: 1 },
+				{ x: 0, y: 1, z: 1 }
+			],
+
+			edges: [
+				{ s: 0, e: 1 },
+				{ s: 1, e: 2 },
+				{ s: 2, e: 3 },
+				{ s: 3, e: 0 },
+
+				{ s: 4, e: 5 },
+				{ s: 5, e: 6 },
+				{ s: 6, e: 7 },
+				{ s: 7, e: 4 },
+
+				{ s: 0, e: 4 },
+				{ s: 1, e: 5 },
+				{ s: 2, e: 6 },
+				{ s: 3, e: 7 },
 			]
-
-
-		}
+		});
 	}
 
 	smooth();
@@ -67,83 +159,49 @@ function draw() {
 
 	clear();
 
-	let horizontalY = height * horizontalLine / 100;
-	let verticalX = width * verticalLine / 100;
-	let leftX = width * leftViewpoint / 100;
-	let rightX = width * (100 + rightViewpoint) / 100;
-	let topY = height * topViewpoint / 100;
-	let bottomY = height * (100 + bottomViewpoint) / 100;
-	
-	let pX = new Array(10);
-	let pY = new Array(10);
-	let pZ = new Array(10);
-
-	let w = 10;
-
-	for (let i = 0; i < 10; i++) {
-		pZ[i] = {
-			x : verticalX,
-			y : topY + (bottomY - topY) / 100 * w * i
-		};
-	}
-
-	for (let z = 0; z < 10; z++) {
+	if (showGridLines.checked) {
 		stroke(0, 0, 0, 64);
 		strokeWeight(1);
-		for (let i = 0; i < 10; i++) {
-			pX[i] = {
-				x : pZ[z].x + (leftX - pZ[z].x) / 150 * w * i,
-				y : pZ[z].y + (horizontalY - pZ[z].y) / 150 * w * i
-			};
-			pY[i] = {
-				x : pZ[z].x + (rightX - pZ[z].x) / 150 * w * i,
-				y : pZ[z].y + (horizontalY - pZ[z].y) / 150 * w * i
-			};
 
-			if (showGridLines.checked) {
-				line(rightX, horizontalY, pX[i].x, pX[i].y);
-				line(leftX, horizontalY, pY[i].x, pY[i].y);
+		for (let z = 0; z < gridSize; z++) {
+			for (let x = 0; x < gridSize; x++) {
+				const g = grid[x][0][z];
+				line(rightX, horizontalY, g.x, g.y);
 			}
-		}
-
-		let g;
-		g = grid[0][0][z];
-		g.x = pZ[z].x;
-		g.y = pZ[z].y;
-		for (let x = 1; x < 10; x++) {
-			g = grid[x][0][z];
-			g.x = pX[x].x;
-			g.y = pX[x].y;
-			for (let y = 1; y < 10; y++) {
-				g = grid[0][y][z];
-				g.x = pY[y].x;
-				g.y = pY[y].y;
-
-				let p = intersectLines(pX[x].x, pX[x].y, 
-					rightX, horizontalY, 
-					pY[y].x, pY[y].y,
-					leftX, horizontalY);
-
-				g = grid[x][y][z];
-				g.x = p.x;
-				g.y = p.y;
+			for (let y = 0; y < gridSize; y++) {
+				const g = grid[0][y][z];
+				line(leftX, horizontalY, g.x, g.y);
 			}
 		}
 	}
 
-	stroke(0);
-	strokeWeight(1);
-	noFill();
-	for (let x = 0; x < 10; x++) {
-		for (let y = 0; y < 10; y++) {
-			for (let z = 0; z < 10; z++) {
-				let g = grid[x][y][z];
-				ellipse(g.x, g.y, 3, 3);
-
-				if (g.b > 0) {
-						
+	if (showGridPoints.checked) {
+		stroke(0);
+		strokeWeight(1);
+		noFill();
+		for (let x = 0; x < gridSize; x++) {
+			for (let y = 0; y < gridSize; y++) {
+				for (let z = 0; z < gridSize; z++) {
+					const g = grid[x][y][z];
+					ellipse(g.x, g.y, 2, 2);
 				}
 			}
+		}
+	}
+
+	stroke(255, 0, 0);
+	strokeWeight(2);
+	for (let object of objects) {
+		for (let vertex of object.vertices) {
+			let g = grid[vertex.x][vertex.y][vertex.z];
+			ellipse(g.x, g.y, 5, 5)
+		}
+		for (let edge of object.edges) {
+			const vs = object.vertices[edge.s];
+			const gs = grid[vs.x][vs.y][vs.z];
+			const ve = object.vertices[edge.e];
+			const ge = grid[ve.x][ve.y][ve.z];
+			line(gs.x, gs.y, ge.x, ge.y);
 		}
 	}
 
@@ -183,12 +241,49 @@ function draw() {
 // --  gui sync  -------------------------------------------------------------
 
 function updateGlobals() {
-	horizontalLine = horizontalLineInput.value * 1.0;
-	verticalLine = verticalLineInput.value * 1.0;
-	leftViewpoint = leftViewPointInput.value * 1.0;
-	rightViewpoint = rightViewPointInput.value * 1.0;
-	topViewpoint = topViewPointInput.value * 1.0;
-	bottomViewpoint = bottomViewPointInput.value * 1.0;
+	let update = false;
+
+	let t = gridSizeInput.value * 1.0;
+	if (t != gridSize) {
+		gridSize = t;
+		updateGridSize();
+		update = true;
+	}
+
+	t = horizontalLineInput.value * 1.0
+	if (t != horizontalLine) {
+		horizontalLine = t;
+		update = true;
+	}
+	t = verticalLineInput.value * 1.0;
+	if (t != verticalLine) {
+		verticalLine = t;
+		update = true;		
+	}
+	t = leftViewPointInput.value * 1.0;
+	if (t != leftViewpoint) {
+		leftViewpoint = t;
+		update = true;		
+	}
+	t = rightViewPointInput.value * 1.0;
+	if (t != rightViewpoint) {
+		rightViewpoint = t;
+		update = true;		
+	}
+	t = topViewPointInput.value * 1.0;
+	if (t != topViewpoint) {
+		topViewpoint = t;
+		update = true;		
+	}
+	t = bottomViewPointInput.value * 1.0;
+	if (t != bottomViewpoint) {
+		bottomViewpoint = t;
+		update = true;		
+	}
+
+	if (update) {
+		updateGrid()
+	}
 }
 
 function updateDisplays() {
@@ -199,6 +294,7 @@ function updateDisplays() {
 	rightViewPointOutput.innerHTML = rightViewpoint.toFixed(2);
 	topViewPointOutput.innerHTML = topViewpoint.toFixed(2);
 	bottomViewPointOutput.innerHTML = bottomViewpoint.toFixed(2);
+	gridSizeOutput.innerHTML = gridSize.toFixed(0);
 }
 
 // ---------------------------------------------------------------------------
@@ -215,5 +311,3 @@ function intersectLines(p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y) {
 		y : p1y + d * (p2y - p1y)
 	};
 }
-
-// --  draw functions  -------------------------------------------------------
